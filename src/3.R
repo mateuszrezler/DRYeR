f88bd9e0f2aff7ab8323165d140017f532fd45c4
Mateusz Rezler <mateuszrezler@gmail.com>
2020-06-10 09:50:02 +0200

    Typo fixed

--- src/3.R
+++ src/3.R
@@ -3,6 +3,6 @@ df <- read.table("../data/FASTA_sample.fa")
 print(df)
 
 cat("\nSelect rows with even indices only\n")
-even_indices <- seq(2, nrow(seq_df), 2)
-print(as.vector(seq_df$V1[even_indices]), quote = FALSE)
+even_indices <- seq(2, nrow(df), 2)
+print(as.vector(df$V1[even_indices]), quote = FALSE)
 
